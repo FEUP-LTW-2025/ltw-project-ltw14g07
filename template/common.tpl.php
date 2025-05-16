c65eaccead5ac9dea432f95181d53b0ba2838478
Micael <micaelrotaru2005@gmail.com>
2025-05-16 19:04:18 +0100

    initial responsiveness on main pages

--- template/common.tpl.php
+++ template/common.tpl.php
@@ -12,6 +12,7 @@
             <link rel="stylesheet" href="../css/style.css">
             <link rel="stylesheet" href="../css/layout.css">
             <link rel="stylesheet" href="../css/responsive.css">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
         </head>
         <body>
             <header>
