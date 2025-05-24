3b400c82fdac8acf5f32000361e9d13390901e70
Micael <micaelrotaru2005@gmail.com>
2025-05-24 13:52:23 +0100

    query selector error console fix

--- js/loadMore.js
+++ js/loadMore.js
@@ -1,20 +1,24 @@
 let offset = 6
 
-document.querySelector("#loadMore").addEventListener("click", function () {
-    console.log("tried to fetch")
-    userID = document.querySelector(".listing").dataset.id
+const button = document.querySelector("#loadMore")
 
-    fetch(`/../action/actionLoadMore.php?offset=${offset}&userID=${userID}`)
-        .then(response => response.text()) 
-        .then(service_cards => {
-            if (service_cards.trim() === 'no more data') {
-                console.log("no more to show")
-                document.querySelector("#loadMore").textContent = "No more Services"
-                document.querySelector("#loadMore").style.backgroundColor = "white"
-                document.querySelector("#loadMore").disabled = true
-                return;
-            }
-            document.querySelector(".listing ul").insertAdjacentHTML('beforeend', service_cards)
-            offset+=6
+if (button) {
+    button.addEventListener("click", function () {
+        console.log("tried to fetch")
+        userID = document.querySelector(".listing").dataset.id
+
+        fetch(`/../action/actionLoadMore.php?offset=${offset}&userID=${userID}`)
+            .then(response => response.text()) 
+            .then(service_cards => {
+                if (service_cards.trim() === 'no more data') {
+                    console.log("no more to show")
+                    button.textContent = "No more Services"
+                    button.style.backgroundColor = "white"
+                    button.disabled = true
+                    return;
+                }
+                document.querySelector(".listing ul").insertAdjacentHTML('beforeend', service_cards)
+                offset+=6
         })
-})
+})}
+
